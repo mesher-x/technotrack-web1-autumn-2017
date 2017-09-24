5e134d49444fd28d1bd64bb2ab9ddb880b3b85fb
Evgeniy Mecheryakov <mesher.x@icloud.com>
2017-09-24 23:50:10 +0300

    do cosmetics

--- httpserver/server/run.py
+++ httpserver/server/run.py
@@ -2,100 +2,111 @@
 import socket
 import datetime
 import os
+import re
 
 
 def parse(request_string):
-	lines = request_string.split('\n')
-	headers = {}
-	headers['request-line'] = lines[0]
-	del lines[0]
-	for line in lines:
-		delimeter = line.find(':')
-		headers[line[:delimeter]] = line[delimeter + 1:].strip()
-	print headers
-	return headers
-
-
-def same_headers():
-	headers = {	'response-line': 'HTTP/1.1 ',
-				'Date': str(datetime.datetime.now()),
-				'Server': 'Macintosh; Intel Mac OS X 10_12_6',
-				'User-Agent': 'Macintosh; Intel Mac OS X 10_12_6',
-				'Content-Type': 'text/html',
-				'Content-Length': '0',
-				'Connection': 'close'}
-	return headers
-
-def glue(response, content):
-	return (response['response-line'] + '\n' + 'Date: ' 
-		+ response['Date'] + '\n'+ 'Server: ' + response['Server'] 
-		+ '\n' + 'Content-Length: ' + response['Content-Length'] + '\n' 
-		+ 'Content-Type: ' + response['Content-Type'] + '\n' + 'Connection: ' 
-		+ response['Connection'] + '\n\n' + content)
-
+    lines = request_string.split('\r\n')
+    request_line_string = lines[0].strip()
+    first_space = request_line_string.find(' ')
+    second_space = request_line_string.rfind(' ')
+    request_line = {'Method': request_line_string[:first_space],
+                    'Uri': request_line_string[first_space + 1:second_space],
+                    'HTTP-Version': request_line_string[second_space + 1:]
+                    }
+    request_headers = {}
+    del lines[0]
+    for line in lines:
+        delimeter = line.find(':')
+        request_headers[line[:delimeter]] = line[delimeter + 1:].strip()
+    return request_line, request_headers
 
 
 def get_response(request_string):
-	headers = parse(request_string)
-	(method, path, protocol) = headers['request-line'].split(' ')
-	response = same_headers()
-	if method == 'GET':
-		if path == '/':
-			response['response-line'] += '200 OK'
-			content = '<html><head></head><body>Hello mister!<br>You are: ' + headers['User-Agent'] + '</body></html>'
-			response['Content-Length'] = str(len(content))
-		elif path == '/media/':
-			response['response-line'] += '200 OK'
-			content = '<html><head></head><body>'
-			files = os.listdir('/files/')
-			for file in files:
-				content += file + '<br>'
-			content += '</body></html>'
-			response['Content-Length'] = str(len(content))
-		elif path.find('/media/') != -1 and len(path) > 7:
-			response['response-line'] += '200 OK'
-			content = '<html><head></head><body>'
-			try:
-				file = open('/files' + path[path.find('a/') + 1:], 'r')
-				content += file.read()
-			except IOError:
-				response['response-line'] = 'HTTP/1.1 404 Not found'
-				content += 'File not found'
-			content += '</body></html>'
-			response['Content-Length'] = str(len(content))
-		elif path == '/test/':
-			response['response-line'] += '200 OK'
-			content = '<html><head></head><body>'
-			content += request_string + '</body></html>'
-			response['Content-Length'] = str(len(content))
-		else:
-			response['response-line'] = 'HTTP/1.1 404 Not found'
-			content = '<html><head></head><body>'
-			content += 'Page not found' + '</body></html>'
-			response['Content-Length'] = str(len(content))
-		return glue(response, content)
+    request_line, request_headers = parse(request_string)
+
+    status_line = {'HTTP-Version': 'HTTP/1.1',
+                   'Code': '200',
+                   'Reason-Phrase': 'OK'
+                   }
+
+    response_headers = {'Date': str(datetime.datetime.now()),
+                        'Server': 'Macintosh; Intel Mac OS X 10_12_6',
+                        'Content-Type': 'text/html',
+                        'Content-Length': '0',
+                        'Connection': 'close'
+                        }
+
+    if request_line['Method'] != 'GET':
+        status_line['Code'] = '404'
+        status_line['Reason-Phrase'] = 'Not found'
+        content = '<html><head></head><body>Page not found</body></html>'
+        response_headers['Content-Length'] = str(len(content))
+
+    elif request_line['Uri'] == '/':
+        status_line['Code'] = '200'
+        status_line['Reason-Phrase'] = 'OK'
+        content = '<html><head></head><body>Hello mister!<br>You are: ' \
+                  + request_headers['User-Agent'] + '</body></html>'
+
+    elif request_line['Uri'] == '/media/':
+        status_line['Code'] = '200'
+        status_line['Reason-Phrase'] = 'OK'
+        content = '<html><head></head><body>'
+        directory_contents = os.listdir('/files/')
+        for name in directory_contents:
+            content += name + '<br>'
+        content += '</body></html>'
+
+    elif request_line['Uri'] == '/test/':
+        status_line['Code'] = '200'
+        status_line['Reason-Phrase'] = 'OK'
+        content = '<html><head></head><body>' + request_string + '</body></html>'
+
+    elif re.search(r'[a-zA-z/]+', request_line['Uri']):
+        status_line['Code'] = '200'
+        status_line['Reason-Phrase'] = 'OK'
+        content = '<html><head></head><body>'
+        try:
+            path = request_line['Uri']
+            file = open('/files' + path[path.find('/', 1):], 'r')
+            content += file.read()
+        except IOError:
+            status_line['Code'] = '404'
+            status_line['Reason-Phrase'] = 'Not found'
+            content += 'File not found'
+        content += '</body></html>'
+
+    response_headers['Content-Length'] = str(len(content))
+    return form_response(status_line, response_headers, content)
 
 
-
-
-
+def form_response(status_line, response_headers, content):
+    return (status_line['HTTP-Version'] + ' ' + status_line['Code'] + ' ' + status_line['Reason-Phrase']
+            + '\r\n' + 'Date: ' + response_headers['Date']
+            + '\r\n' + 'Server ' + response_headers['Server']
+            + '\r\n' + 'Content-Type ' + response_headers['Content-Type']
+            + '\r\n' + 'Content-Length ' + response_headers['Content-Length']
+            + '\r\n' + 'Connection ' + response_headers['Connection']
+            + '\r\n\r\n' + content
+            )
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 8000))  #server_socket привязывается к address = (ip, port)
-server_socket.listen(1)  #создает очередь размера 0 на входящие соединения(если очередь заполнена, новые соединения не принимаются)
+server_socket.bind(('localhost', 8000))  # server_socket привязывается к address = (ip, port)
+server_socket.listen(0)  # создает очередь размера 0 на входящие соединения (сколько можно держать not accepted)
 
 print 'Started'
 
 while 1:
     try:
         (client_socket, address) = server_socket.accept()
-        print 'Got new client', client_socket.getsockname()  #возвращает адрес клиента (tuple)
-        request_string = client_socket.recv(2048) #блокируясь если нечего читать, читает максимум 2048 байт
-        response = get_response(request_string)
-        client_socket.send(response)  #посылает строку 
-    except KeyboardInterrupt:  #в случае нажатия ctrl-c
+        print 'Got new client', client_socket.getsockname()  # возвращает адрес клиента (tuple)
+        request_string = client_socket.recv(2048) # блокируясь если нечего читать, читает максимум 2048 байт
+        client_socket.send(get_response(request_string))  # посылает строку
+        client_socket.close()
+    except KeyboardInterrupt:  # в случае нажатия ctrl-c
         print 'Stopped'
-        server_socket.close()  #закрывает server_socket, если этот сокет более никем не испоьзуется освобождаются ресурсы
+        server_socket.close()  # закрывает server_socket, если этот сокет более никем не испоьзуется освобождаются ресурсы
         exit()
